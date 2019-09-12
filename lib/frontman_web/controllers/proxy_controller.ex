fee37b978b8f4fb18bab97f8a80ed93ac7b7b294
Eric Teubert <eric@ericteubert.de>
2019-09-12 11:41:33 +0200

    handle etag/if-none-match

--- lib/frontman_web/controllers/proxy_controller.ex
+++ lib/frontman_web/controllers/proxy_controller.ex
@@ -14,12 +14,25 @@ defmodule FrontmanWeb.ProxyController do
       feed = %Feed{} ->
         fetch_feed(feed.origin_url)
         |> case do
-          {:ok, xml} ->
-            conn
-            |> put_status(200)
-            |> put_resp_header("cache-control", "max-age=300, public, must-revalidate")
-            |> Plug.Conn.put_resp_content_type("application/xml", "utf-8")
-            |> html(xml)
+          {:ok,
+           %{
+             xml: xml,
+             etag: etag,
+             last_modified: last_modified
+           }} ->
+            case get_req_header(conn, "if-none-match") do
+              [request_etag] when request_etag == etag ->
+                send_resp(conn, 304, "")
+
+              _ ->
+                conn
+                |> put_status(200)
+                |> put_resp_header("cache-control", "max-age=300, public, must-revalidate")
+                |> put_resp_header("etag", etag)
+                |> put_resp_header("last-modified", last_modified)
+                |> Plug.Conn.put_resp_content_type("application/xml", "utf-8")
+                |> html(xml)
+            end
 
           {:error, other} ->
             html(conn, "found #{inspect(other)}")
@@ -37,15 +50,22 @@ defmodule FrontmanWeb.ProxyController do
         do_fetch_feed(url)
         |> case do
           {:ok, xml} ->
-            ConCache.put(:feeds, url, xml)
-            {:ok, xml}
+            data = %{
+              xml: xml,
+              etag: etag(xml),
+              last_modified: last_modified(xml)
+            }
+
+            ConCache.put(:feeds, url, data)
+
+            {:ok, data}
 
           {:error, reason} ->
             {:error, reason}
         end
 
-      xml ->
-        {:ok, xml}
+      data ->
+        {:ok, data}
     end
   end
 
@@ -66,5 +86,46 @@ defmodule FrontmanWeb.ProxyController do
       {:error, %HTTPoison.Error{reason: reason}} ->
         {:error, reason}
     end
+  end
+
+  defp etag(xml), do: "W/" <> Base.encode16(:crypto.hash(:md5, xml), case: :lower)
+
+  defp last_modified(_), do: DateTime.utc_now() |> to_rfc7232()
+
+  def to_rfc7232(datetime) do
+    date = DateTime.to_date(datetime)
+
+    day_name =
+      case Date.day_of_week(date) do
+        1 -> "Mon"
+        2 -> "Tue"
+        3 -> "Wed"
+        4 -> "Thu"
+        5 -> "Fri"
+        6 -> "Sat"
+        7 -> "Sun"
+      end
+
+    month_name =
+      case date.month do
+        1 -> "Jan"
+        2 -> "Feb"
+        3 -> "Mar"
+        4 -> "Apr"
+        5 -> "May"
+        6 -> "Jun"
+        7 -> "Jul"
+        8 -> "Aug"
+        9 -> "Sep"
+        10 -> "Oct"
+        11 -> "Nov"
+        12 -> "Dec"
+      end
+
+    time = datetime |> DateTime.to_time() |> Time.truncate(:second) |> Time.to_iso8601()
+
+    "#{day_name}, #{date.day |> Integer.to_string() |> String.pad_leading(2, "0")} #{month_name} #{
+      date.year
+    } #{time} GMT"
   end
 end
